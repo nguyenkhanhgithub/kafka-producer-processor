a1b510c2af702045d1bfb02d7bdb3a3c7ed88e7d
nguyenkhanh <nguyenkhanh29121995@gmail.com>
2022-09-30 20:20:22 +0700

    fixed

--- src/main/java/com/cmcglobal/kafkastreamprocessor/KafkaStreamProcessorApplication.java
+++ src/main/java/com/cmcglobal/kafkastreamprocessor/KafkaStreamProcessorApplication.java
@@ -1,5 +1,6 @@
 package com.cmcglobal.kafkastreamprocessor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -9,9 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableKafka
 @EnableScheduling
+@Slf4j
 public class KafkaStreamProcessorApplication {
     public static void main(String[] args) {
         SpringApplication.run(KafkaStreamProcessorApplication.class, args);
+        log.info("aaaaaaaaa");
     }
 
 }

9bf491337314adcf7cb0db51fcfc32be0c24a39b
nguyenkhanh <nguyenkhanh29121995@gmail.com>
2022-10-03 09:53:56 +0700

    fixed

--- src/main/java/com/cmcglobal/kafkastreamprocessor/controllers/TestController.java
+++ src/main/java/com/cmcglobal/kafkastreamprocessor/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.cmcglobal.kafkastreamprocessor.controllers;
 
+import io.sentry.Sentry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,11 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<?> getAll() {
+        try {
+            throw new Exception("This is a test.");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
         return ResponseEntity.ok(true);
     }
 }
